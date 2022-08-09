25a881d5834ea8c02b17650bb1e3e364f8718f04
Hoshiyar Singh <hoshiyarjyani@gmail.com>
2022-08-10 00:05:39 +0530

    half page complete till counter and responsive css

--- main.js
+++ main.js
@@ -1,3 +1,19 @@
+// active Navbar
+
+let nav = document.querySelector(".navigation-wrap");
+window.onscroll = function(){
+    if(document.documentElement.scrollTop > 20){
+        nav.classList.add("scroll-on");
+    }else{nav.classList.remove("scroll-on");
+}
+}
+
+
+
+
+
+
+
 // counter design 
 
 document.addEventListener("DOMContentLoaded", () => {
