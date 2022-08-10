8ff3f5fcdb6340e1dbb8ee5b353ab5e6d36b768d
Hoshiyar Singh <hoshiyarjyani@gmail.com>
2022-08-11 02:31:46 +0530

    Page complete html css js bootstrap

--- main.js
+++ main.js
@@ -9,7 +9,14 @@ window.onscroll = function(){
 }
 
 
-
+// nav hide 
+let navBar = document.querySelectorAll('.nav-link');
+let navCollapse = document.querySelector('.navbar-collapse.collapse');
+navBar.forEach(function(a){
+    a.addEventListener("click", function(){
+        navCollapse.classList.remove("show");
+    })
+})
 
 
 
